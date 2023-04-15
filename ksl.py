29485f8187cb58cd49e0c7940b75c54b68773b83
Paul <dev@wfi>
2023-04-15 17:05:08 -0600

    cleanup

--- ksl.py
+++ ksl.py
@@ -134,9 +134,6 @@ def RecordProperties(PropertyList, Name):
         for p in PropertyList:
             writer.writerow(p.GetReportRow())
 
-#<div class="GridItem_GridItemTitle__k35xf">473 13th Street, Ogden, UT 84404</div><div class="GridItem_GridItemTitle__k35xf" style="visibility: hidden; height: 0px; margin-bottom: 0px;"><span>473 13th Street, Ogden, UT 84404</span></div><div class="GridItem_GridItemPrice__Cdf3J">$449,900</div><div class="GridItem_GridItemDetail__VRLk2"><span>4 bed</span><span> | 3 bath</span><span> | 2,931 sqft</span></div>    
-#<a class="GridItem_GridItemInner__LOPea" href="/listing/40514347" target="_blank"><div class="GridItem_GridItemTitle__k35xf">473 13th Street, Ogden, UT 84404</div><div class="GridItem_GridItemTitle__k35xf" style="visibility: hidden; height: 0px; margin-bottom: 0px;"><span>473 13th Street, Ogden, UT 84404</span></div><div class="GridItem_GridItemPrice__Cdf3J">$449,900</div><div class="GridItem_GridItemDetail__VRLk2"><span>4 bed</span><span> | 3 bath</span><span> | 2,931 sqft</span></div></a>
-
 def UnitTest_Ksl():
     ksl = Ksl()
     print(ksl)
